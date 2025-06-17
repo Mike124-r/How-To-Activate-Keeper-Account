bd2f0278d525a2499cad24b2d1990302cbb7cab5
Mike124-r <tontnoel@outlook.com>
2025-06-17 19:14:55 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'How-To-Activate-Keeper-Account'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
